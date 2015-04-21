c1d67fa1727d6b6634ca20b89b36b049a8dcde80
Ty <tyler@faceyspacies.com>
2015-04-21 13:59:31 -0500

    plus includes/IndexHelper.php since totes forgot it

---
+++ includes/IndexHelper.php
@@ -0,0 +1,41 @@
+<?php
+
+	function show_submission($content_id, $username, $content_title, $content, $date) {
+		?><li class="well">
+			<h2 class="submission-title"><?php echo($content_title) ?></h2>
+			
+			<p><?php echo($content) ?></p>
+			
+			<div class="text-muted text-right">-- <?php echo($username) ?>, <?php echo($date) ?></div>
+			
+			<p class="text-right;"><a href="/index.php?id=<?php echo($content_id)?>" title="Permalink">Permalink</a></p>
+		
+		</li><?php
+		
+	}
+	
+	function get_submissions($offset, $limit) {
+		$submissions = array();
+		// http://php.net/manual/en/mysqli.query.php
+		
+		global $db_host, $db_user, $db_password, $db_name;
+		$db_handler = new mysqli($db_host, $db_user, $db_password, $db_name);
+		
+		// totes sql injection problem spot, should totes fix later
+		$sql = "SELECT * FROM submissions WHERE submissionId > " . $offset . " LIMIT " . $limit;
+		
+		if($result = $db_handler->query($sql)) {
+			
+			while($row = $result->fetch_row()) {
+				$submissions[] = array (
+					"submissionId" => $row[0],
+					"title" => $row[1],
+					"content" => $row[2],
+					"userId" => $row[3],
+					"date" => $row[4]
+				);
+			}
+		}
+		
+		return $submissions;
+	}
