614eb90419f98d01924eed3116eb0295ff659801
Taylor <taylor@faceyspacies.com>
2015-04-23 14:04:28 -0500

    Delete works
    The Delete page now Deletes

--- delete.php
+++ delete.php
@@ -1,7 +1,35 @@
 <!DOCTYPE html>
 	<?php
 		require_once("includes/AutoLoader.php");
+		require_once("includes/DeleteHelper.php");
+		
 		displayHeader( "Admin" );
+		if(!isset($_SESSION['isAdmin']) || !$_SESSION['isAdmin']) { //gives error message if not admin
+		displayErrorMessage( "You are not allowed to view this page.");
+	} elseif (isset($_SESSION['isAdmin']) && $_SESSION['isAdmin']) { //if admin it will show the page
+	
+		if( $_SERVER['REQUEST_METHOD'] === 'POST' ) {
+			global $db_host, $db_user, $db_password, $db_name;
+			$hasError = false;
+			$db_handler = mysqli_connect($db_host, $db_user, $db_password, $db_name);
+			
+			if( $db_handler->connect_error ) {
+				displayErrorMessage("Unable to connect to Database, please try again later.");
+				$hasError = true;
+				if(!isset($_POST['submissionID']) && empty($_POST['submissionID'])) {
+					displayErrorMessage("Please provide submissionID and <a href=\"delete.php\">try again</a>");
+					$hasError = true;
+				}
+			}
+							
+			if(!$hasError) {
+				$id = deleteSubmission($_POST['submissionID'],  $db_handler);
+				
+				if($id !== false ) {
+					displaySuccessMessage("Submission successfully deleted");
+				} 
+			}
+		} 
 	?> 		
 	
 		<div class="container">
@@ -17,4 +45,5 @@
 			</form>
 		</div>
 <?php
+}
 displayFooter();
---
+++ includes/DeleteHelper.php
@@ -0,0 +1,22 @@
+<?php
+
+	function deleteSubmission( $submissionID, $db_handler ) {
+		// http://php.net/manual/en/mysqli.quickstart.prepared-statements.php
+		if(!($prepared_statement = $db_handler->prepare("DELETE FROM submissions WHERE submissionID = $submissionID"))) {
+			displayErrorMessage("Database error, please try again later.");
+			return false;
+		} // end prepare sql statement 
+		
+		// 's' shows that $username is a string
+		//if(!($prepared_statement->bind_param( 'sss', $submissionID, $_SESSION['userId'] ))) {
+		//	displayErrorMessage("Database error, please try again later.");
+		//	return false;
+		//} // end bind params to statement
+		
+		if(!$prepared_statement->execute()) {
+			displayErrorMessage("Database error, please try again later.");
+			return false;
+		} // end actually perform query
+		
+		return $prepared_statement->insert_id;
+	}
