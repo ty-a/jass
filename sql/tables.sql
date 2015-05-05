be631c6682b9d9873669e29414a9b22eb09677d5
Ty <tyler@faceyspacies.com>
2015-05-05 13:10:28 -0500

    fix typo

--- sql/tables.sql
+++ sql/tables.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS user(
 	userName varchar(15) NOT NULL,
 	password varchar(250) NOT NULL,
 	isAdmin boolean DEFAULT FALSE,
-	disabled boolean DEFAULT FALSE.
+	disabled boolean DEFAULT FALSE
 );
 
 CREATE TABLE IF NOT EXISTS submissions(
